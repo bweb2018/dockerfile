ee66df67c6654b01228cb9fc7f0052ea7a3ef0b5
v-hub <v-hui@microsoft.com>
2019-08-02 15:53:40 +0800

    dockerfile-generator

---
+++ precache-manifest.9eaeb778ae893c856153c2983a1f391b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc223eb018593677c408eb8c401c3276",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cba1e6173ac4451c8e48",
+    "url": "/static/css/2.8242e397.chunk.css"
+  },
+  {
+    "revision": "62bbeae3610cbbff5a79",
+    "url": "/static/css/main.163a1500.chunk.css"
+  },
+  {
+    "revision": "cba1e6173ac4451c8e48",
+    "url": "/static/js/2.815a3755.chunk.js"
+  },
+  {
+    "revision": "62bbeae3610cbbff5a79",
+    "url": "/static/js/main.82a3dabc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

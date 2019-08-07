97f00cd2ad4a283e40e14560714fae375820ba20
v-hub <v-hui@microsoft.com>
2019-08-07 15:04:52 +0800

    add code

---
+++ precache-manifest.d641ae0cb291f4ca32360efa13dcd622.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feab03b440a8cd46a2e4",
+    "url": "bundle.js"
+  },
+  {
+    "revision": "2214c2b65c56e1fc826408a95ad85186",
+    "url": "index.html"
+  }
+]);
